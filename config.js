dfde729a7f6de132b7eb9c7636f7b8f2fe01c693
BuiNgocTam10096 <85400345+BuiNgocTam10096@users.noreply.github.com>
2021-06-06 22:47:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘  😘😘',
     messDesc: 'Tối mai 7h anh qua đón nhé công chúa',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' 
-}
+
+
