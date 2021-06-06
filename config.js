c6b20770a338d808705aa7483e1b21238389b0e9
BuiNgocTam10096 <85400345+BuiNgocTam10096@users.noreply.github.com>
2021-06-06 22:08:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
 const CONFIG = {
-    introTitle: 'Hiếu ơi!',
-    introDesc: `Hiếu hãy trả lời thiệt nghe Hiếu`,
+    introTitle: 'Em à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Hiếu đẹp trai hay xấu trai 😙',
-    desc: 'chỉ được chọn 1 đáp án thôi nhé ',
-    btnYes: 'Xấu trai bẩn tính <33',
-    btnNo: 'Đẹp trai nhưng khổ :3',
-    question:'Tại sao Hiếu đẹp trai ri mà không có ai mê nhỉ <3',
-    btnReply: 'Để Tâm nói cho Hiếu biết nè <3',
-    reply: 'Hiếu nói dữ quá <33333333',
-    mess: 'ĐÚNG KHÔNG  😘😘',
-    messDesc: 'Tối mai đi nhậu nhé',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy, chỉ được chon 1 đáp án thôi nhé ',
+    btnYes: 'Vẫn cứ là thích anh  <33',
+    btnNo: Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu  <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘  😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
